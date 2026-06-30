8351a04414c49658427e4d5aa937b0733dd50109
Hasnat <hasnathasin.67@gmail.com>
2026-06-30 15:47:15 +0600

    first commit before DVC

---
+++ my_code.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+import os
+
+data={
+
+  "name": ["Hasnat","Maruf","Momin"],
+  "Age":[23,24,22],
+  "address": ["bog","raj","kus"]
+
+}
+
+
+df=pd.DataFrame(data)
+
+
+# adding new row 
+# df.log[len(df.index)]={"name": "V2","Age":25,"address": "nator"}
+
+
+# adding new row 
+# df.log[len(df.index)]={"name": "V3","Age":30,"address": "city1"}
+
+
+
+# make dir 
+
+os.makedirs("data",exist_ok=True)
+
+file_path=os.path.join("data","sample_data.csv")
+
+df.to_csv(file_path,index=False)
+
+print(f"CSV file saved to {file_path}")
+
